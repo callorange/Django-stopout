9b6511b70d6042732d1488e9428978898c1ac4fb
jeongyeob <sjy20000@gmail.com>
2018-02-04 19:59:41 +0900

    db 마이그레이션 작업 및 view 연동

--- django/webtoon/models.py
+++ django/webtoon/models.py
@@ -4,17 +4,27 @@ from django.utils.dateparse import parse_date
 class Webtoon(models.Model):
     webtoon_no = models.IntegerField(default=0)
     title = models.CharField(max_length=200)
-    thumbnail = models.CharField(max_length=300)
+    thumbnail = models.CharField(max_length=300) # static 경로로 넣을것.
     author = models.CharField(max_length=20)
     description = models.CharField(max_length=100)
     url = models.CharField(max_length=100)
+
+    def get_episode_list(self):
+        pass
+
+    def __str__(self):
+        return self.title
 
 
 class Episode(models.Model):
     webtoon = models.ForeignKey(Webtoon, on_delete=models.CASCADE)
     webtoon_no = models.IntegerField(default=0)
     episode_no = models.IntegerField(default=0)
-    thumbnail = models.CharField(max_length=300)
-    rating = models.CharField(max_length=3)
+    title = models.CharField(max_length=200, default='')
+    thumbnail = models.CharField(max_length=300) # static 경로로 넣을것.
+    rating = models.CharField(max_length=4)
     created_date = models.DateField('published date')
     url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "{} - {}".format(self.webtoon, self.title)
--- django/webtoon/views.py
+++ django/webtoon/views.py
@@ -1,10 +1,35 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Webtoon, Episode
 
 def index(request):
-    return render(request, 'webtoon/index.html')
+    """웹툰 목록을 보여준다"""
+    try:
+        search_webtoon = request.GET['search_webtoon']
+    except Exception as e:
+        search_webtoon = None
+
+    if search_webtoon:
+        toons = Webtoon.objects.filter(title__contains = search_webtoon)
+    else:
+        toons = Webtoon.objects.all()
+
+    context = {
+        'toons': toons,
+        'search_webtoon': search_webtoon,
+    }
+    return render(request, 'webtoon/index.html', context)
 
 
 def episode_list(request, webtoon_id):
-    return render(request, 'webtoon/episode_list.html')
+    """웹툰의 에피소드 목록을 보여준다"""
+    toon = Webtoon.objects.filter(pk=webtoon_id)
+    ep = Episode.objects.filter(webtoon=webtoon_id)
+    if not toon.exists() or not ep.exists():
+        return redirect('webtoon:index')
+
+    context = {
+        'webtoon': toon.get(),
+        'ep': ep,
+    }
+    return render(request, 'webtoon/episode_list.html', context)
